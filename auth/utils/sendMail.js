175dcc0065a3806d858b2b475c783eef2febaa58
magzumAlmat <almat.magzum@gmail.com>
2025-11-18 17:30:51 +0500

    changed nodemailer settings

--- auth/utils/sendMail.js
+++ auth/utils/sendMail.js
@@ -4,14 +4,14 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'almat.magzum@gmail.com', // Замените на свой адрес электронной почты Gmail
-    pass: 'wxhprftlxvgljeyn' // Замените на свой пароль от электронной почты Gmail
+    user: 'almatmagzum8@gmail.com', // Замените на свой адрес электронной почты Gmail
+    pass: 'italex586rtflf43D123456D!' // Замените на свой пароль от электронной почты Gmail
   }
 });
 
 function sendEmail(to,subject,text) {
     const mailOptions = {
-      from: 'almat.magzum@gmail.com', 
+      from: 'almatmagzum8@gmail.com', 
       to: to, 
       subject: subject,
       html:text,
