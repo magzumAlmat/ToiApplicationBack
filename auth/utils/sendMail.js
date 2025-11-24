b18d1a2a32d04b421f2346b3cecfff8784081963
magzumAlmat <almat.magzum@gmail.com>
2025-11-24 10:27:10 +0500

    24 nov changed node mailer acc

--- auth/utils/sendMail.js
+++ auth/utils/sendMail.js
@@ -1,3 +1,44 @@
+// const nodemailer = require('nodemailer');
+
+// // Создаем транспорт для отправки писем через Gmail SMTP
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'almat.magzum@gmail.com', // Замените на свой адрес электронной почты Gmail
+//     pass: 'wxhprftlxvgljeyn' // Замените на свой пароль от электронной почты Gmail
+//   }
+// });
+
+// function sendEmail(to,subject,text) {
+//     const mailOptions = {
+//       from: 'almat.magzum@gmail.com', 
+//       to: to, 
+//       subject: subject,
+//       html:text,
+//     };
+//   // Отправляем письмо
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email отправлен: ' + info.response);
+//     }
+//   });
+// }
+
+// module.exports=sendEmail;
+
+
+
+
+
+
+
+
+
+
+
+
 const nodemailer = require('nodemailer');
 
 // Создаем транспорт для отправки писем через Gmail SMTP
@@ -5,7 +46,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'almatmagzum8@gmail.com', // Замените на свой адрес электронной почты Gmail
-    pass: 'italex586rtflf43D123456D!' // Замените на свой пароль от электронной почты Gmail
+    pass: '586rtflf43DDD!' // Замените на свой пароль от электронной почты Gmail
   }
 });
 
